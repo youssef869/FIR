d162c81fb730a5677de7fb5503135c47ff97e863
Youssef Khaled <109810438+youssef869@users.noreply.github.com>
2025-04-15 01:28:26 +0200

    Delete multiplier.v

--- multiplier.v
+++
@@ -1,9 +0,0 @@
-module multiplier #(parameter WIDTH = 16)(
-input  wire [WIDTH-1: 0]  sig,
-input  wire [WIDTH-1: 0]  coeff,
-output wire [2*WIDTH-1: 0] out
-);
-
-assign out = sig * coeff;
-
-endmodule
